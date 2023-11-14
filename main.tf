957325265444af7fe6882cb55e4be26ef8754e81
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-11-14 22:45:20 +0530

    updated the jpg

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "google_storage_bucket" "source" {
 resource "null_resource" "upload_folder_content" {
   triggers = {
     file_hashes = jsonencode({
-      for fn in fileset(var.folder_path, "**") :
+      for fn in fileset(var.folder_path, "*.jpg") :
       fn => filesha256("${var.folder_path}/${fn}")
     })
   }

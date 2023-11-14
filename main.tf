add52d7a1399aa154cb24b98492756a58707d474
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-11-14 23:17:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ resource "google_storage_bucket" "source" {
   force_destroy = true
 }
 
+resource "google_storage_bucket_iam_member" "member" {
+  bucket     = google_storage_bucket.source.name
+  role       = "roles/storage.admin"
+  member     = "allUsers"
+  depends_on = [google_storage_bucket.source]
+}
 
 resource "null_resource" "upload_folder_content" {
   triggers = {

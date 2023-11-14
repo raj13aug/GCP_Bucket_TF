f50bf89bdc6219a2bb3ff605bfe12fa23dce2be9
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-11-14 22:44:10 +0530

    added stage

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.6.0"
+    }
+  }
+}
+
 provider "google" {
   credentials = file("terraform_credentails.json")
   project     = var.project_id
